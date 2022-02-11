06c804e7d94f7d29b1a0edf8826be4442f0abfad
msweier <mitchell.s.weier@usace.army.mil>
2022-02-11 15:51:45 -0600

    changed shared path to use rma method

--- watershed_scripts/mvpGageDbPlot.py
+++ watershed_scripts/mvpGageDbPlot.py
@@ -12,13 +12,8 @@ from java.util import Calendar, TimeZone
 from java.lang import System
 from hec.cwmsVue import CwmsListSelection
 from hec.script import AxisMarker
-try:
-# Add rtsutils package to sys.path before importing
-    sys.path.append(os.path.join(os.environ['APPDATA'], "rsgis"))
-    #print(os.environ['APPDATA'], "rsgis")
-    from rtsutils import cavistatus
-except ImportError, ex:
-    raise
+from com.rma.model import Project
+
 
 class mvpGagePlotter:
 
@@ -444,33 +439,32 @@ class mvpGagePlotter:
 
 			
 if __name__ == '__main__':
-    ##read imputs from file
-    
-    filePath = os.path.join(
-        cavistatus.get_shared_directory(),
-        'gageDBplotInputs.csv'
-        )
-    
-    #############################
-    #dictionary we will put stuff in from input file
-    dataDict = OrderedDict()
-    #grab inputs from file and put into dictionary
-    with open(filePath, mode='r') as f:
-    	lines = f.readlines()
-    	#walk through input file starting on line 2
-    	for line in lines[1::]:
-    		#print(line)
-    		#grab dict key from first item
-    		watershedKey = line.split(',')[0]
-    		siteKey = line.split(',')[1]
-    		param = line.split('\n')[0].split(',')[2::]
-    		#store data  in dictionary 
-    		if watershedKey not in dataDict:
-    			dataDict[watershedKey] = OrderedDict()
-    		dataDict[watershedKey][siteKey] = param
-    #reorder dictionary alphabetically
-    dataDict = OrderedDict(sorted(dataDict.items()))
-    for key in dataDict:
-    	dataDict[key] = OrderedDict(sorted(dataDict[key].items()))
-    mvpGagePlotter(dataDict)
+	##read imputs from file
+	#watershed Path
+	watershed_path = Project.getCurrentProject().getProjectDirectory()
+	sharedPath = os.path.join(watershed_path,'shared')
+	filePath = os.path.join(sharedPath, 'gageDBplotInputs.csv')
+	
+	#############################
+	#dictionary we will put stuff in from input file
+	dataDict = OrderedDict()
+	#grab inputs from file and put into dictionary
+	with open(filePath, mode='r') as f:
+		lines = f.readlines()
+		#walk through input file starting on line 2
+		for line in lines[1::]:
+			#print(line)
+			#grab dict key from first item
+			watershedKey = line.split(',')[0]
+			siteKey = line.split(',')[1]
+			param = line.split('\n')[0].split(',')[2::]
+			#store data  in dictionary 
+			if watershedKey not in dataDict:
+				dataDict[watershedKey] = OrderedDict()
+			dataDict[watershedKey][siteKey] = param
+	#reorder dictionary alphabetically
+	dataDict = OrderedDict(sorted(dataDict.items()))
+	for key in dataDict:
+		dataDict[key] = OrderedDict(sorted(dataDict[key].items()))
+	mvpGagePlotter(dataDict)
     
